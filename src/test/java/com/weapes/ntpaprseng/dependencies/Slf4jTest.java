a22f5722be0298ed9c68019089859327b068f373
huixianghuang <916705608@qq.com>
2016-08-13 13:44:12 +0800

    Better slf4j and log4j

--- src/test/java/com/weapes/ntpaprseng/dependencies/Slf4jTest.java
+++ src/test/java/com/weapes/ntpaprseng/dependencies/Slf4jTest.java
@@ -4,6 +4,7 @@ package com.weapes.ntpaprseng.dependencies;
 import java.beans.PropertyEditor;
 import java.util.logging.Logger;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.*;
@@ -16,8 +17,13 @@ public class Slf4jTest {
     @Ignore
     public void slf4jTest(){
 
+        //日志变量
         org.slf4j.Logger logger =   LoggerFactory.getLogger(Slf4jTest.class);
 
+        //加载log4j配置文件
+        PropertyConfigurator.configure("E:\\javaproject\\git_project\\NTPaprSEng\\conf\\log4j.properties");
+
+        //打印日志
         logger.info("nice ------ Hello");
         logger.warn("warn ++++++ warn");
 
