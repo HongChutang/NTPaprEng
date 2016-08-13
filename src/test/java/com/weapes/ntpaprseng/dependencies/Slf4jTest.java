1d076c780ac33302746fe705ab9827c94df2d8f1
huixianghuang <916705608@qq.com>
2016-08-13 11:25:25 +0800

    another commit for slf4j

--- src/test/java/com/weapes/ntpaprseng/dependencies/Slf4jTest.java
+++ src/test/java/com/weapes/ntpaprseng/dependencies/Slf4jTest.java
@@ -1,12 +1,12 @@
 package com.weapes.ntpaprseng.dependencies;
 
 
+import java.beans.PropertyEditor;
 import java.util.logging.Logger;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.*;
-import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Created by Programmer on 2016/8/12.
@@ -16,9 +16,7 @@ public class Slf4jTest {
     @Ignore
     public void slf4jTest(){
 
-        org.slf4j.Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
-
-        PropertyConfigurator.configure("E:\\javaproject\\git_project\\NTPaprSEng\\conf\\log4j.properties");
+        org.slf4j.Logger logger =   LoggerFactory.getLogger(Slf4jTest.class);
 
         logger.info("nice ------ Hello");
         logger.warn("warn ++++++ warn");
